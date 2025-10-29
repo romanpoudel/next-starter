43fd41f21d4b9027b66a4dc517a9e154ea3bd7db
Roman Poudel <romanpoudel325@gmail.com>
2025-10-29 09:27:49 +0545

    feat: build in standalone

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import path from "node:path";
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
   turbopack: {
     root: path.join(__dirname, ".."),
   },
